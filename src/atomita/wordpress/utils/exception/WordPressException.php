59303d59c45c8f73883b931579d5ce79cf6b5a3e
atomita <sleeping.cait.sith+gh@gmail.com>
2016-03-25 17:54:44 +0900

    add WordPressException.

---
+++ src/atomita/wordpress/utils/exception/WordPressException.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace atomita\wordpress\utils\exception;
+
+use Exception;
+use WP_Error;
+
+class WordPressException extends Exception {
+	protected $error;
+
+	function __construct(WP_Error $error){
+		$code = $error->get_error_codes();
+		parent::__construct($error->get_error_message($code), intval($code));
+
+		$this->error = $error;
+	}
+
+	function getError(){
+		return $this->error;
+	}
+
+	function __get($name){
+		return $this->error->$name;
+	}
+
+	function __set($name, $value){
+		$this->error->$name = $value;
+	}
+
+	function __call($method, $args){
+		return call_user_func_array([$this->error, $method], $args);
+	}
+}
